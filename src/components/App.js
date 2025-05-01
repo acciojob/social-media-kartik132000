6015f6203770bf0e0e0b8d2bbec807203338fee9
kartik132000 <sujeetkumarsen37@gmail.com>
2025-05-01 13:02:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,47 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./app/store";
-import PostsList from "./features/posts/PostsList";
-import AddPostForm from "./features/posts/AddPostForm";
-import PostDetail from "./features/posts/PostDetail";
-import EditPostForm from "./features/posts/EditPostForm";
-import UsersList from "./features/users/UsersList";
-import UserPage from "./features/users/UserPage";
-import NotificationsList from "./features/notifications/NotificationsList";
-import Navbar from "./components/Navbar";
-import "./../styles/App.css";
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 
-const App = () => {
+import { Navbar } from './app/Navbar'
+
+import { PostsList } from './features/posts/PostsList'
+import { AddPostForm } from './features/posts/AddPostForm'
+import { EditPostForm } from './features/posts/EditPostForm'
+import { SinglePostPage } from './features/posts/SinglePostPage'
+import { UsersList } from './features/users/UsersList'
+import { UserPage } from './features/users/UserPage'
+import { NotificationsList } from './features/notifications/NotificationsList'
+
+function App() {
   return (
-    <div>
-      {/* Do not remove the main div */}
-      <Provider store={store}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<PostsList />} />
-            <Route path="/posts/:postId" element={<PostDetail />} />
-            <Route path="/editPost/:postId" element={<EditPostForm />} />
-            <Route path="/users" element={<UsersList />} />
-            <Route path="/users/:userId" element={<UserPage />} />
-            <Route path="/notifications" element={<NotificationsList />} />
-          </Routes>
-        </Router>
-      </Provider>
-    </div>
-  );
-};
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+          <Route exact path="/notifications" component={NotificationsList} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
 
-export default App;
+export default App
